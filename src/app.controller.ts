b4d8f7b69276e05672c0fb6f760dc116e9e06c3e
herious <909578167@qq.com>
2021-03-16 22:09:02 +0800

    配置 swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
 
 @Controller()
+@ApiTags('主路由')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'root'
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
+  const listenPort = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Eagle School')
+    .setDescription('The eagle school API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  console.log(`you application  is running hear: localhost:${listenPort}`)
+  await app.listen(listenPort);
+
 }
 bootstrap();
