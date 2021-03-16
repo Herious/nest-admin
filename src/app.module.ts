cd1807948baa27eb59bb9741774e0a7f3d11ceb4
herious <909578167@qq.com>
2021-03-16 22:30:46 +0800

    配置log4js模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Log4jsModule } from '@nestx-log4js/core'
 
 @Module({
-  imports: [],
+  imports: [
+    Log4jsModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Log4jsLogger } from '@nestx-log4js/core';
+import { Logger } from '@nestjs/common';
 
 
 async function bootstrap() {
   const listenPort = 3000;
   const app = await NestFactory.create(AppModule);
+  /**
+   * 配置 Swagger API
+   */
   const options = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Eagle School')
-    .setDescription('The eagle school API description')
+    .setDescription('项目管理接口文档')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  console.log(`you application  is running hear: localhost:${listenPort}`)
+
+  /**
+   * 使用log4js日志模块替换默认日志
+   */
+  app.useLogger(app.get(Log4jsLogger));
+  Logger.log(`you application  is running hear: localhost:${listenPort}`)
   await app.listen(listenPort);
 
 }
