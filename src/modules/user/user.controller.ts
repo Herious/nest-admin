6cbec0aceb7585d506400efb9186b2fcb3193292
herious <909578167@qq.com>
2021-03-18 23:17:38 +0800

    新增swagger接口注释

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto'
 
@@ -9,11 +9,17 @@ export class UserController {
   constructor(private readonly UserService: UserService) {}
 
   @Post('regist')
+  @ApiOperation({
+    summary: '用户注册接口'
+  })
   async regist(@Body() user: UserDTO) {
     return await this.UserService.regist(user)
   }
 
   @Post('login')
+  @ApiOperation({
+    summary: '用户登录接口'
+  })
   async login(@Body() user: UserDTO) {
     return await this.UserService.login(user)
   }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -58,37 +58,23 @@ export class UserService {
 
 
 	regExpValidation(param: string, reg: RegExp): boolean {
-		if (reg.test(param)) {
-			return true;
-		} else {
-			return false;
-		}
+		return reg.test(param);
 	}
 
 	checkPhone(val: string): boolean {
-		if(val == '') {
-			return false;
-		}
 		let phoneReg = /^[1][3,4,5,7,8][0-9]{9}$/;
 		return this.regExpValidation(val, phoneReg);
 	}
 
 	checkCarId(val: string): boolean {
-		if(val == '') {
-			return false
-		}
 		var carIdReg = /(^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}|[A-Z0-9]{5}[A-Z0-9挂学警港澳]{1}$)/;
 		return this.regExpValidation(val, carIdReg);
 	}
 
 	checkIdCard(val: string): boolean {
-		if(val == '') {
-			return false
-		}
 		var carIdReg = /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
 		return this.regExpValidation(val, carIdReg);
 	}
-
 
 }
 
